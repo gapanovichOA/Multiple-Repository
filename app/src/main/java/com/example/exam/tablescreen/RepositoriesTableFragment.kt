e3f9e1dade3663b75a0e9eb313176f4535b9c322
gapanovichOA <volha.hapanovich@innowise-group.com>
2022-08-24 11:47:59 +0300

    added appbar

--- app/src/main/java/com/example/exam/tablescreen/RepositoriesTableFragment.kt
+++ app/src/main/java/com/example/exam/tablescreen/RepositoriesTableFragment.kt
@@ -3,14 +3,19 @@ package com.example.exam.tablescreen
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.MenuRes
+import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.domain.model.RepositoryModel
 import com.example.exam.BaseFragment
+import com.example.exam.R
 import com.example.exam.databinding.FragmentRepositoriesTableBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -42,6 +47,29 @@ class RepositoriesTableFragment : BaseFragment<FragmentRepositoriesTableBinding>
                 }
             }
             repositoriesList.layoutManager = LinearLayoutManager(view.context)
+            topAppBar.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.search -> {
+                        true
+                    }
+                    R.id.more -> {
+                        true
+                    }
+                    R.id.option_1->{
+                        true
+                    }
+                    R.id.option_2->{
+                        true
+                    }R.id.option_3->{
+                        true
+                    }R.id.option_4->{
+                        true
+                    }R.id.option_5->{
+                        true
+                    }
+                    else -> false
+                }
+            }
         }
     }
 
